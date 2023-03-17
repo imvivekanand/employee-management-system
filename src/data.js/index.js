d7e96be764623ff8d47d7fbeb5280aa12bb1230c
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-03-17 18:32:47 +0530

    added data

---
+++ src/data.js/index.js
@@ -0,0 +1,12 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: "Vivek",
+    LastName: "Anand",
+    email: "vivekanand@onmail.com",
+    salary: "100000000251",
+    date: "2023-02-11",
+  },
+];
+
+export default employeesData;
